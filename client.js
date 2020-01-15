3982b3d6edc40babdd87a6af6626cf427d7ca35b
Ken Fyfe <kenfyfe@kenfyfe.com>
2020-01-15 23:18:49 +0000

    added constants

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require('net');
+const {IP, PORT} = require('./constants');
 /*
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
@@ -16,23 +17,6 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: KRF');
-  });
-
-  conn.on('connect', () => {
-    // setTimeout(() => {
-    //   conn.write('Move: up');
-    // }, 1000);
-    // setTimeout(() => {
-    //   conn.write('Move: left');
-    // }, 2000);
-    // setTimeout(() => {
-    //   conn.write('Move: up');
-    // }, 3000);
-    // setTimeout(() => {
-    //   setInterval(() => {
-    //     conn.write('Move: right');
-    //   }, 100);
-    // }, 4000);
   });
 
   return conn;
